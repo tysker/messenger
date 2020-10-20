a8bf12e82c4e75df59521fa152e596e5c13d0e0f
tysker <oerteljoerg@gmail.com>
2020-10-20 09:55:13 +0200

    added two nye functions til MessageService

--- src/main/java/dk/oertel/service/MessageService.java
+++ src/main/java/dk/oertel/service/MessageService.java
@@ -4,10 +4,7 @@ import dk.oertel.database.DatabaseClass;
 import dk.oertel.model.Message;
 
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MessageService {
 
@@ -22,6 +19,23 @@ public class MessageService {
 
     public List<Message> getAllMessages() {
         return new ArrayList<Message>(messages.values());
+    }
+
+    public List<Message> getAllMessagesForYear(int year){
+        List<Message> messagesForYear = new ArrayList<>();
+        Calendar cal = Calendar.getInstance();
+        for(Message message : messages.values()) {
+            cal.setTime(message.getCreated());
+            if(cal.get(Calendar.YEAR) == year) {
+                messagesForYear.add(message);
+            }
+        }
+        return messagesForYear;
+    }
+
+    public List<Message> getAllMessagesPaginated(int start, int size) {
+        ArrayList<Message> list = new ArrayList<Message>(messages.values());
+        return list.subList(start, start + size);
     }
 
     public Message getMessage(long id) {
